7b8e2783fad3d51dec02d6f088506e0cd090e6af
Richard Rosario <tech2js@gmail.com>
2020-08-19 10:15:18 +0800

    creating direct messages from users to users

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -17,6 +17,7 @@ class SidePanel extends React.Component {
       >
         <UserPanel currentUser={currentUser} />
         <Channels currentUser={currentUser} />
+        <DirectMessages />
       </Menu>
     );
   }
